edf737107f687dc8561e5838bbea803a7815fd05
kacpertrolejbus <kruszelnickik@gmail.com>
2025-02-03 20:07:36 +0100

    update

--- demozaikowanie.py
+++ demozaikowanie.py
@@ -28,35 +28,30 @@ apply_filter(image_gray, blur, 'Rozmywanie obrazu')
 apply_filter(image_gray, sharpen, 'Wyostrzanie obrazu')
 
 # Definicja maski Bayera
-bayer_mask = np.array([[[0, 1], [0, 0]],  # Czerwony
-                        [[1, 0], [0, 1]],  # Zielony
-                        [[0, 0], [1, 0]]], dtype=np.uint8)  # Niebieski
-bayer_mask = np.transpose(bayer_mask, axes=(1, 2, 0))
+bayer_mask = np.zeros((2, 2, 3))
+bayer_mask[0, 0, 0] = 1  # Czerwony
+bayer_mask[0, 1, 1] = 1  # Zielony
+bayer_mask[1, 0, 1] = 1  # Zielony
+bayer_mask[1, 1, 2] = 1  # Niebieski
 
-# Funkcja do tworzenia CFA dla obrazu
-def color_filter_array(mask, shape):
-    return np.dstack([
-        np.tile(mask[:, :, channel], np.asarray(shape) // len(mask[:, :, channel]))
-        for channel in range(mask.shape[-1])
-    ])
-
-# Tworzenie filtru Bayera
-bayer_filter = color_filter_array(bayer_mask, np.array([1024, 1024]))
+# Powielenie maski Bayera na cały obraz
+bayer_filter = np.tile(bayer_mask, (image.shape[0] // 2, image.shape[1] // 2, 1))
 
 # Symulacja obrazu z czujnika
 sensor_image = image * bayer_filter
 
-# Filtr konwolucyjny do demozaikowania
-demosaicking_convolution_mask = np.dstack([
-    np.array([[0, 0.25, 0], [0.25, 1, 0.25], [0, 0.25, 0]]),  # R
-    np.array([[0.5, 1, 0.5], [1, 4, 1], [0.5, 1, 0.5]]) / 4,  # G
-    np.array([[0, 0.25, 0], [0.25, 1, 0.25], [0, 0.25, 0]])   # B
-])
+# Filtry interpolacyjne dla demozaikowania
+interp_kernels = {
+    'red': np.array([[0.25, 0.5, 0.25], [0.5, 1, 0.5], [0.25, 0.5, 0.25]]),
+    'green': np.array([[0, 0.25, 0], [0.25, 1, 0.25], [0, 0.25, 0]]),
+    'blue': np.array([[0.25, 0.5, 0.25], [0.5, 1, 0.5], [0.25, 0.5, 0.25]])
+}
 
 # Implementacja demozaikowania
 reconstructed_image = np.dstack([
-    convolve(sensor_image[:, :, channel], demosaicking_convolution_mask[:, :, channel], mode="nearest")
-    for channel in range(3)
+    convolve(sensor_image[:, :, 0], interp_kernels['red'], mode="nearest"),
+    convolve(sensor_image[:, :, 1], interp_kernels['green'], mode="nearest"),
+    convolve(sensor_image[:, :, 2], interp_kernels['blue'], mode="nearest")
 ])
 
 # Normalizacja wartości do przedziału [0, 1]
@@ -64,7 +59,7 @@ reconstructed_image = np.clip(reconstructed_image, 0, 1)
 
 # Wizualizacja wyniku demozaikowania
 plt.figure(figsize=(6, 6))
-plt.imshow(reconstructed_image)  # Wyświetlenie w kolorze
+plt.imshow(reconstructed_image)
 plt.title('Demozaikowanie - Filtr Bayera')
 plt.axis('off')
 plt.show()
